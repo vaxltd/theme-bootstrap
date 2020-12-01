f82c414bb4cf03d25133c18f6424e00fda04fc66
Gary Christian <gchristian@tti-fc.com>
2020-12-01 14:25:03 +0000

    change namespace from webgriffe to vaxltd

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Webgriffe/bootstrap',
+    'frontend/VaxLtd/bootstrap',
     __DIR__
 );
